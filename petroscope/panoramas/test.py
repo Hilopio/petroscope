82d2222c31cd61ad2e511f50ab82bbfde233248a
Hilopio <Nickolaev.Gleb03@yandex.ru>
2025-07-05 21:04:58 +0000

    sth

--- petroscope/panoramas/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-print("hello" * np.sum([1, 2, 3]))
-
-print("hello" * np.sum([1, 2, 3]))
-print("hello" * np.sum([1, 2, 3]))
-print("hello" * np.sum([1, 2, 3]))
-print("hello" * np.sum([1, 2, 3]))
